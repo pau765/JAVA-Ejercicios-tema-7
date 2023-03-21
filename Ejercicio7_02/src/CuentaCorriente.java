58b3226cc9d903ebc3881ad53cbfe22fefeda6f0
PauPau <126180139+pau765@users.noreply.github.com>
2023-03-21 16:52:07 +0100

    Add files via upload

--- Ejercicio7_02/src/CuentaCorriente.java
+++ Ejercicio7_02/src/CuentaCorriente.java
@@ -1,4 +1,40 @@
 
 public class CuentaCorriente {
+    String dni;
+    String nombre;
+    double saldo;
 
+    CuentaCorriente(String dni, String nombre) {
+        this.dni = dni;
+        this.nombre = nombre;
+    }
+    CuentaCorriente (String dni, double saldo){
+        this.dni = dni;
+        this.saldo = saldo;
+        this.nombre = "Sin asignar";
+    }
+    CuentaCorriente (String dni, String nombre, double saldo) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.saldo = saldo;
+     }
+    void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("DNI: " + dni);
+        System.out.println("Saldo: " + saldo + " euros");
+    }
+    void ingreso(double ingCant) {
+        saldo += ingCant;
+    }
+   boolean egreso(double sacarCant) {
+        boolean operacionPosible = false;
+        if (saldo >= sacarCant) {
+            saldo -= sacarCant ;
+            operacionPosible = true;
+        } else if (saldo == 0){
+            System.out.print("No hay dinero suficiente");
+            operacionPosible = false;
+        }
+        return (operacionPosible);
+    }
 }
