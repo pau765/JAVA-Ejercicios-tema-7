e91da110040b11309a6a9dcd476f2542acbcf6eb
PauPau <126180139+pau765@users.noreply.github.com>
2023-03-22 20:18:55 +0100

    Add files via upload

---
+++ Ejercicio7_12/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Lista l1 = new Lista();
+        Lista l2 = new Lista();
+        l1.insertarFinal(1); l1.insertarFinal(1); l1.insertarFinal(2);l1.insertarFinal(3);
+        l2.insertarFinal(10);l2.insertarFinal(20);l2.insertarFinal(30);
+        Lista concatenacion = Lista.concatena(l1, l2);
+        concatenacion.mostrar();
+    }
+}
